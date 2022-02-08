dccdc742979fc263faa5b17fe56ea82b70b871c5
MubashirWaheed <mubashir3080@gmail.com>
2022-02-08 16:19:21 +0500

    refactored code

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,41 +1,40 @@
 import css from '../css/feed.module.css'
 import { useEffect, useState } from 'react';
 import { db } from '../Firebase/config';
-import { collection, onSnapshot, getDocs, query, where } from 'firebase/firestore';
+import { collection, onSnapshot, getDocs, query, where,orderBy } from 'firebase/firestore';
 
 const Feed = ()=>{
     const [tweets , setTweets] = useState([]);
     const following = [];
     const uid = JSON.parse(localStorage.getItem("currentUser")).user.uid;
-    // console.log(uid);
-    const getFollowing = async ()=>{
-        const q = query(collection(db, "users"), where("uid", "==", uid));
-        const querySnapshot = await getDocs(q);
-        querySnapshot.forEach((doc)=>{
-            following.push(doc.data().following);
-            // console.log(doc.data().following);
-        })
-    }
-    
-    const getTweets = ()=>{
-        console.log(following);
-        const q = query(collection(db, "tweets"), where("creatorId", "in", following[0]));
-        onSnapshot(q, (querySnapshot) => {
-            setTweets([]);
-            querySnapshot.forEach((doc)=>{
-                setTweets((testtweets)=>{
-                    return [...testtweets, doc.data()]
-                })
-            })
-        })
-    }
-
+ 
     useEffect(()=>{
+        let unsub;
         const getData = async ()=>{
-            await getFollowing();
-            getTweets();
+            // Reading following array from firestore if following array empty
+            if(following.length < 1 ){
+                const q = query(collection(db, "users"), where("uid", "==", uid));
+                const querySnapshot = await getDocs(q);
+                querySnapshot.forEach((doc)=>{
+                    following.push(doc.data().following);
+                });
+            };
+            // Querying tweets from firestore
+            const k = query(collection(db, "tweets"), orderBy("timeStamp", "desc"),where("creatorId", "in", following[0]));
+            unsub = onSnapshot(k, (snapshot) => {
+                setTweets([]);
+                snapshot.forEach((doc)=>{
+                    setTweets((testtweets)=>{
+                        return [...testtweets, doc.data()]
+                    });
+                });
+            });
         }
-        getData()
+        
+        getData();
+
+        // removing listener when feed component is unmounted
+        return ()=> unsub?.();
     },[])
     
     return (
@@ -43,8 +42,8 @@ const Feed = ()=>{
             {
                 tweets.map((item)=>{
                     return (
-                        <p key={item.id}>{item.tweet}</p>
-                    )
+                        <p key={item.uuid}>{item.tweet}</p>
+                    );
                 })
             }
             <h1>Feed</h1>
