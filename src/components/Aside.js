10fcc4191171c033362149d79a60424a086eab70
MubashirWaheed <mubashir3080@gmail.com>
2022-02-09 19:13:24 +0500

    designed aside component

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,9 +1,20 @@
 import css from '../css/aside.module.css'
+import avi from '../images/twitter-login.png'
 const Aside = ()=>{
     return(
         <div className={css.container}>
-            <h1> Aside</h1>
-
+            <input className={css.search} type="text" title='Disabled for now' disabled placeholder='Search' />
+            <div className={css.wrapper}>
+                <h2>Who to follow</h2>
+                <div className={css.users}>
+                    <img  className={css.avi} src={avi} alt="avi" />
+                    <div className={css.user}>
+                        <h4>Jason Roy</h4>
+                        <p>@JasonRoy</p>
+                    </div>
+                    <button className={css.follow}>Follow</button>
+                </div>
+            </div>
         </div>
     )
 }
